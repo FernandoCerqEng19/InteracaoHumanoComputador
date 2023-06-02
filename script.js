ce3a198379851ef52802190711a5b3dbb73afbbc
FernandoCerqEng19 <fernando11cerqueira@gmail.com>
2023-06-02 04:45:28 -0300

    Add 1

---
+++ script.js
@@ -0,0 +1,27 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.area-1', { duration: 1000 });
+
+sr.reveal('.area-2', { duration: 1000 });
+
+sr.reveal('.area-3', { duration: 1000 });
+
+sr.reveal('.area-4', { duration: 1000 });
+
+sr.reveal('.module', { duration: 1000 });
+
+window.addEventListener('scroll', function(){
+    let header = document.querySelector('header')
+    header.classList.toggle('rolagem', window.scrollY > 30)
+})
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+    var h1 = document.getElementById('ititle');
+  
+    if (window.pageYOffset > header.offsetHeight) {
+      h1.classList.add('hide');
+    } else {
+      h1.classList.remove('hide');
+    }
+})
